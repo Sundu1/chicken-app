984c60e56479dd30255b3d605ddef3e898cd1248
sundui <sundui.gal63@gmail.com>
2023-03-10 11:38:28 +0800

    commit

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import './components/NavBar'
+import NavBar from './components/NavBar';
+import SideBar from './components/SideBar'
+
+function App() {
+  return (
+    <div className="text-center">
+      <NavBar/>
+      <SideBar/>
+      test
+    </div>
+  );
+}
+
+export default App;

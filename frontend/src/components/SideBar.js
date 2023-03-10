d310f9ca7888b305585bd4782c6c8ca4de799dc1
sundui <sundui.gal63@gmail.com>
2023-03-10 16:26:02 +0800

    sidebar commit

--- frontend/src/components/SideBar.js
+++ frontend/src/components/SideBar.js
@@ -1,7 +1,7 @@
 import { React, useEffect, useState } from "react";
 
 const SideBar = () => {
-  const [openProfile, setOpenProfile] = useState(false);
+  const [open, setOpen] = useState(false);
   const [buttons, setButtons] = useState([
     {
       Name: "PROFILE OVERVIEW",
@@ -95,8 +95,46 @@ const SideBar = () => {
     },
   ]);
 
-  const openSideBarFunction = (e) => {
-    setOpenProfile(!openProfile);
+  const SideBarMenu = ({ button }) => {
+    const [subnav, setSubnav] = useState(false);
+
+    const showSubnav = () => setSubnav(!subnav);
+
+    return (
+      <div
+        key={button.Name}
+        className="flex flex-col justify-start items-center px-6 border-b border-gray-600 w-full"
+      >
+        <button
+          value={button.Name}
+          onClick={showSubnav}
+          className="z-50 focus:outline-none focus:text-indigo-400 text-left  text-white flex justify-between items-center w-full py-5 space-x-14  "
+        >
+          <p className="pointer-events-none text-sm leading-5  uppercase">
+            {button.Name}
+          </p>
+        </button>
+        <div
+          id="menu1"
+          className={
+            subnav
+              ? "flex justify-start  flex-col w-full md:w-auto items-start pb-1"
+              : "hidden"
+          }
+        >
+          {button.values.map((value) => {
+            return (
+              <button
+                key={value.field}
+                className="flex justify-start items-center space-x-6 hover:text-white focus:bg-gray-700 focus:text-white hover:bg-gray-700 text-gray-400 rounded px-3 py-2  w-full md:w-52"
+              >
+                <p className="text-base leading-4  ">{value.field}</p>
+              </button>
+            );
+          })}
+        </div>
+      </div>
+    );
   };
 
   return (
@@ -191,33 +229,7 @@ const SideBar = () => {
         </div>
         <div className="no-scrollbar overflow-y-auto px-6 border-b border-gray-600 w-full">
           {buttons.map((button) => {
-            return (
-              <div className="flex flex-col justify-start items-center px-6 border-b border-gray-600 w-full">
-                <button
-                  value={button.Name}
-                  onClick={(e) => openSideBarFunction(e)}
-                  className="z-50 focus:outline-none focus:text-indigo-400 text-left  text-white flex justify-between items-center w-full py-5 space-x-14  "
-                >
-                  <p className="text-sm leading-5  uppercase">{button.Name}</p>
-                </button>
-                <div
-                  id="menu1"
-                  className={
-                    openProfile
-                      ? "flex justify-start  flex-col w-full md:w-auto items-start pb-1"
-                      : "hidden"
-                  }
-                >
-                  {button.values.map((value) => {
-                    return (
-                      <button className="flex justify-start items-center space-x-6 hover:text-white focus:bg-gray-700 focus:text-white hover:bg-gray-700 text-gray-400 rounded px-3 py-2  w-full md:w-52">
-                        <p className="text-base leading-4  ">{value.field}</p>
-                      </button>
-                    );
-                  })}
-                </div>
-              </div>
-            );
+            return <SideBarMenu button={button} key={button.Name} />;
           })}
         </div>
       </div>

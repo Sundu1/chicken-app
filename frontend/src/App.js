4cede1543f61732ab67b2df2518ca0d8648532f2
sundui <sundui.gal63@gmail.com>
2023-03-10 14:55:05 +0800

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,65 @@
-import './App.css';
-import './components/NavBar'
-import NavBar from './components/NavBar';
-import SideBar from './components/SideBar'
+import "./App.css";
+import "./components/NavBar";
+import NavBar from "./components/NavBar";
+import SideBar from "./components/SideBar";
 
 function App() {
   return (
     <div className="text-center">
-      <NavBar/>
-      <SideBar/>
-      test
+      <div className="block flex">
+        <SideBar />
+        <NavBar />
+      </div>
+
+      <h1>sdfsd</h1>
+      <h1>sdfsd</h1>
+      <h1>sdfsd</h1>
+      <h1>sdfsd</h1>
+      <h1>sdfsd</h1>
+      <h1>sdfsd</h1>
+      <h1>sdfsd</h1>
+      <h1>sdfsd</h1>
+      <h1>sdfsd</h1>
+      <h1>sdfsd</h1>
+      <h1>sdfsd</h1>
+      <h1>sdfsd</h1>
+      <h1>sdfsd</h1>
+      <h1>sdfsd</h1>
+      <h1>sdfsd</h1>
+      <h1>sdfsd</h1>
+      <h1>sdfsd</h1>
+      <h1>sdfsd</h1>
+      <h1>sdfsd</h1>
+      <h1>sdfsd</h1>
+      <h1>sdfsd</h1>
+      <h1>sdfsd</h1>
+      <h1>sdfsd</h1>
+      <h1>sdfsd</h1>
+      <h1>sdfsd</h1>
+      <h1>sdfsd</h1>
+      <h1>sdfsd</h1>
+      <h1>sdfsd</h1>
+      <h1>sdfsd</h1>
+      <h1>sdfsd</h1>
+      <h1>sdfsd</h1>
+      <h1>sdfsd</h1>
+      <h1>sdfsd</h1>
+      <h1>sdfsd</h1>
+      <h1>sdfsd</h1>
+      <h1>sdfsd</h1>
+      <h1>sdfsd</h1>
+      <h1>sdfsd</h1>
+      <h1>sdfsd</h1>
+      <h1>sdfsd</h1>
+      <h1>sdfsd</h1>
+      <h1>sdfsd</h1>
+      <h1>sdfsd</h1>
+      <h1>sdfsd</h1>
+      <h1>sdfsd</h1>
+      <h1>sdfsd</h1>
+      <h1>sdfsd</h1>
+      <h1>sdfsd</h1>
+      <h1>sdfsd</h1>
     </div>
   );
 }
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const NavBar = () => {
-  return (
-    <div>NavBar</div>
-  )
-}
+  return <div className="fixed top-0 left-[256px] bg-gray-300">NavBar</div>;
+};
 
-export default NavBar
+export default NavBar;
